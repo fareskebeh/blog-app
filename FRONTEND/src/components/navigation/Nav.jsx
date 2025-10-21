418bbd015a4242a1bcb9bd09dd46cb193bb0c6e0
fareskebeh <fares.kebbeh@gmail.com>
2025-10-21 12:16:19 +0300

    Nav login button

--- FRONTEND/src/components/navigation/Nav.jsx
+++ FRONTEND/src/components/navigation/Nav.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import { HiOutlineSun, HiOutlineMoon } from "react-icons/hi";
 import { useTheme } from '../../hooks/useTheme';
+import { useAuth } from '../../hooks/useAuth';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   const {theme, changeTheme} = useTheme()
+  const{user}= useAuth()
   return (
-    <div className='fixed z-[999] inset-x-0 top-0 p-6 mb-8 flex justify-between bg-neutral-100 dark:bg-neutral-950 transition duration-150'>
-      <button className='cursor-pointer' onClick={changeTheme}>{
-        theme ==="dark" ? <HiOutlineMoon size={24} color='#707070'/> : <HiOutlineSun size={24}/>
-        }</button>
+    <div className='fixed z-[999] inset-x-0 top-0 p-6 mb-8 flex justify-between items-center bg-neutral-100 dark:bg-neutral-950 transition duration-150'>
+      <div/>
+      <div className='flex items-center gap-4'>
+        {
+          user ? <></> : <Link to={"/"} className='p-2 transition duration-150 hover:opacity-95 bg-black text-sm text-white dark:text-black dark:bg-white rounded-xl cursor-pointer'>Log In</Link>
+        }
+          <button className='cursor-pointer' onClick={changeTheme}>{
+            theme ==="dark" ? <HiOutlineMoon size={28} color='#707070'/> : <HiOutlineSun size={28}/>
+          }</button>
+      </div>
     </div>
   )
 }
