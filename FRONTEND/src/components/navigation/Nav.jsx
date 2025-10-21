6166f82156f096057b7dce7278e2f010ddc147d2
fareskebeh <fares.kebbeh@gmail.com>
2025-10-21 11:46:18 +0300

    Fixed the navbar onto the viewport

--- FRONTEND/src/components/navigation/Nav.jsx
+++ FRONTEND/src/components/navigation/Nav.jsx
@@ -5,7 +5,7 @@ import { useTheme } from '../../hooks/useTheme';
 const Nav = () => {
   const {theme, changeTheme} = useTheme()
   return (
-    <div className='p-6 mb-8 flex justify-between bg-neutral-100 dark:bg-neutral-950 transition duration-150'>
+    <div className='fixed z-[999] inset-x-0 top-0 p-6 mb-8 flex justify-between bg-neutral-100 dark:bg-neutral-950 transition duration-150'>
       <button className='cursor-pointer' onClick={changeTheme}>{
         theme ==="dark" ? <HiOutlineMoon size={24} color='#707070'/> : <HiOutlineSun size={24}/>
         }</button>
