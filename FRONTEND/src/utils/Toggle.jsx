e26c29142bd1032f5a14668b8ad2b4c369e85d1b
fareskebeh <fares.kebbeh@gmail.com>
2025-10-27 13:13:02 +0300

    Edited Toggle.jsx for background handling

--- FRONTEND/src/utils/Toggle.jsx
+++ FRONTEND/src/utils/Toggle.jsx
@@ -4,7 +4,7 @@ const Toggle = () => {
   return (
     <div className='flex gap-2'>
         <input className='peer' type="checkbox" hidden id="checkbox" />
-        <label htmlFor='checkbox' className={`h-6 w-10 peer-checked:before:translate-x-4 peer-checked:dark:bg-blue-500 rounded-xl before:transition transition duration-150 relative before:content-[''] before:h-4 before:w-4 before:absolute before:rounded-xl before:bg-white before:top-1 before:left-1 bg-neutral-200 dark:bg-neutral-900 cursor-pointer`}>
+        <label htmlFor='checkbox' className={`h-6 w-10 peer-checked:before:translate-x-4 peer-checked:dark:bg-blue-500 rounded-xl before:transition transition duration-150 relative before:content-[''] before:h-4 before:w-4 before:absolute before:rounded-xl peer-checked:bg-blue-500 before:bg-white before:top-1 before:left-1 bg-neutral-400 dark:bg-neutral-900 cursor-pointer`}>
 
         </label>
     </div>
