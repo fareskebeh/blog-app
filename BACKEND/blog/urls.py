022a28ca593c15d178e34bcd8eeab7828bce56d8
fareskebeh <fares.kebbeh@gmail.com>
2025-10-28 21:46:47 +0300

    Blacklisted the original registration url path

--- BACKEND/blog/urls.py
+++ BACKEND/blog/urls.py
@@ -3,7 +3,10 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from dj_rest_auth.views import PasswordResetView, PasswordResetConfirmView
-from .views import Confirmation
+from .views import Confirmation, RegistrationView
+from dj_rest_auth.urls import urlpatterns as registerURLs
+
+registerURLs.pop(0)
 
 
 urlpatterns = [
@@ -12,6 +15,7 @@ urlpatterns = [
     path('auth/password/reset/', PasswordResetView.as_view()),
     path('auth/registration/account-confirm-email/<str:key>/', Confirmation.as_view()),
     path('auth/password/reset/confirm/<str:uidb64>/<str:token>/', PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('auth/registration/', include(registerURLs)),
+    path('auth/register', RegistrationView.as_view()),
     path("", include("api.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
