6a3c005fb14589bff5c12dab56328f9483a17090
fareskebeh <fares.kebbeh@gmail.com>
2025-10-30 17:30:53 +0300

    Registration page

--- FRONTEND/src/authPages/Register.jsx
+++ FRONTEND/src/authPages/Register.jsx
@@ -1,31 +1,99 @@
-import { HiOutlineUser, HiOutlineAtSymbol, HiOutlineLockClosed, HiOutlineEye, HiOutlineEyeOff } from "react-icons/hi"
+import { HiOutlineUser, HiOutlineAtSymbol, HiOutlineBookOpen, HiOutlineLockClosed, HiOutlineEye, HiOutlineEyeOff } from "react-icons/hi"
 import Toggle from "../utils/Toggle"
 import { useState } from "react"
 import { Link } from "react-router-dom"
+import axiosInit from "../services/axios-init"
 
 const Register = () => {
   const [pwVis, setPwVis] = useState(false)
+  const[credentials,setCredentials] = useState({
+    username: "",
+    email:"",
+    password1:"",
+    password2:""
+  })
+  const[response,setResponse] = useState({
+    status: undefined,
+    message: "",
+  })
+  const validator = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+  const submitForm = (e)=> {
+    e.preventDefault()
+
+    if(!credentials.email||!credentials.password1||!credentials.password2||!credentials.username) {
+      setResponse({
+        status: "error",
+        message: "Required fields missing"
+      })
+      return;
+    }
+    if(!validator.test(credentials.email)) {
+      setResponse({
+        status: "error",
+        message: "Invalid email format"
+      })
+      return;
+    }
+    if(credentials.password1.length < 8) {
+      setResponse({
+        status: "error",
+        message: "Password must be at least 8 characters"
+      })
+      return;
+    }
+    if(credentials.password1!==credentials.password2) {
+      setResponse({
+        status: "error",
+        message: "Passwords do not match"
+      })
+      return;
+    }
+
+    console.log(credentials)
+    setResponse({
+      status: undefined,
+      message: ""
+    })
+    axiosInit.post(`${import.meta.env.VITE_API_BASE}auth/registration/`, credentials, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res=> {
+      console.log(res)
+    })
+    .catch(err=>{
+      console.log(err)
+      setResponse({
+        status: "error",
+        message: "Internal Server Error"
+      })
+    })
+
+  }
+
   return (
     <div className='pt-20 h-dvh flex items-center justify-center dark:bg-neutral-950 bg-neutral-100 transition **:transition duration-150'>
       
-      <div className='flex flex-col gap-12'>
+      <div className='flex flex-col gap-8'>
         <p className='text-3xl dark:text-white font-black'>Create a new account</p>
 
         <div className='flex flex-col gap-2'>
 
-          <div className="relative">
-            <HiOutlineUser size={20} className="text-neutral-500 dark:text-neutral-600 absolute top-2.5 left-2"/>
-            <input className='dark:bg-neutral-900 w-full pl-9 bg-neutral-200 rounded-xl p-2 dark:text-white placeholder-neutral-500 dark:placeholder-neutral-600 outline-none' placeholder='First Name' type="text" />
-          </div>
+            <div className="relative">
+              <HiOutlineBookOpen size={20} className="text-neutral-500 dark:text-neutral-600 absolute top-2.5 left-2"/>
+              <input onChange={(e)=> setCredentials({...credentials, username:e.target.value})} value={credentials.username} className='dark:bg-neutral-900 w-full pl-9 bg-neutral-200 rounded-xl p-2 dark:text-white placeholder-neutral-500 dark:placeholder-neutral-600 outline-none' placeholder='Username' type="text" />
+            </div>
 
           <div className="relative">
             <HiOutlineAtSymbol size={20} className="text-neutral-500 dark:text-neutral-600 absolute top-2.5 left-2"/>
-            <input className='dark:bg-neutral-900 w-full pl-9 bg-neutral-200 rounded-xl p-2 dark:text-white placeholder-neutral-500 dark:placeholder-neutral-600 outline-none' placeholder='E-mail' type="text" />
+            <input onChange={(e)=> setCredentials({...credentials, email:e.target.value})} value={credentials.email} className='dark:bg-neutral-900 w-full pl-9 bg-neutral-200 rounded-xl p-2 dark:text-white placeholder-neutral-500 dark:placeholder-neutral-600 outline-none' placeholder='E-mail' type="text" />
           </div>
           
           <div className="relative">
             <HiOutlineLockClosed size={20} className="text-neutral-500 dark:text-neutral-600 absolute top-2.5 left-2"/>
-            <input className='dark:bg-neutral-900 w-full pl-9 bg-neutral-200 rounded-xl p-2 dark:text-white placeholder-neutral-500 dark:placeholder-neutral-600 outline-none' placeholder='Password' type={pwVis ? "text" : "password"} />
+            <input onChange={(e)=> setCredentials({...credentials, password1:e.target.value})} value={credentials.password1} className='dark:bg-neutral-900 w-full pl-9 bg-neutral-200 rounded-xl p-2 dark:text-white placeholder-neutral-500 dark:placeholder-neutral-600 outline-none' placeholder='Password' type={pwVis ? "text" : "password"} />
             <button onClick={()=> setPwVis(!pwVis) } className={`${pwVis ? "text-neutral-700 dark:text-neutral-500" : "dark:text-neutral-600 text-neutral-500"} transition  duration-150 absolute cursor-pointer top-2.5 right-3`}>
               {pwVis ? <HiOutlineEye size={20}/> : <HiOutlineEyeOff size={20}/>}
             </button>
@@ -33,14 +101,15 @@ const Register = () => {
 
           <div className="relative">
             <HiOutlineLockClosed size={20} className="text-neutral-500 dark:text-neutral-600 absolute top-2.5 left-2"/>
-            <input className='dark:bg-neutral-900 w-full pl-9 bg-neutral-200 rounded-xl p-2 dark:text-white placeholder-neutral-500 dark:placeholder-neutral-600 outline-none' placeholder='Confirm Password' type={pwVis ? "text" : "password"} />
+            <input onChange={(e)=> setCredentials({...credentials, password2:e.target.value})} value={credentials.password2} className='dark:bg-neutral-900 w-full pl-9 bg-neutral-200 rounded-xl p-2 dark:text-white placeholder-neutral-500 dark:placeholder-neutral-600 outline-none' placeholder='Confirm Password' type={pwVis ? "text" : "password"} />
           </div>
           
           <div className="flex gap-2 my-2 items-center">
             <span className="text-neutral-700 text-nowrap dark:text-neutral-500">Remember me</span> 
             <Toggle />
           </div>
-          <button className='p-2  dark:text-black bg-black dark:bg-white text-white rounded-xl cursor-pointer hover:opacity-90'>Register</button>
+          <button onClick={submitForm} className='p-2  dark:text-black bg-black dark:bg-white text-white rounded-xl cursor-pointer hover:opacity-90'>Register</button>
+          <p className={`h-4 ${response.status==="error" ? "text-red-500" : "text-green-600"} ${response.message ? "opacity-100" : "opacity-0"} transition duration-150`}>{response.message}</p>
         </div>
 
         <Link to="/login-with-email" className="flex gap-2 items-center justify-center dark:text-neutral-500"><HiOutlineUser/>Already registered? Log in instead.</Link>
