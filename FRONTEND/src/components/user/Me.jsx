54cb0c397cc1cb97847dd0ecf0b681a4f642bb1f
fareskebeh <fares.kebbeh@gmail.com>
2025-10-31 17:58:57 +0300

    ui tweaks

--- FRONTEND/src/components/user/Me.jsx
+++ FRONTEND/src/components/user/Me.jsx
@@ -24,16 +24,17 @@ const Me = ({user}) => {
   },[profileInfo])
 
   return (
-    <div className='min-h-dvh pt-24 px-4 flex flex-col **:transition duration-150 md:flex-row'>
-      <div className='flex gap-4 flex-col items-center md:flex-row'>
+    <div className='min-h-dvh md:*:flex-1 pt-24 px-4 gap-8 flex flex-col **:transition duration-150 md:flex-row'>
+      <div className='flex gap-4 flex-col items-center md:items-start md:flex-row'>
         <img className='rounded-full w-30 sm:w-40 md:w-50' src={profileInfo?.avatar ? profileInfo.avatar : Avatar}/>
         <div className='space-y-2 text-center md:text-left'>
-          <p className='dark:text-white text-black text-2xl sm:text-3xl md:text-4xl'>@{user?.username}</p>
-          <p className='dark:text-neutral-700 text-lg sm:text-xl md:text-2xl'>See your account info</p>
+          <p className='dark:text-white text-black text-2xl font-bold sm:text-3xl md:text-4xl'>@{user?.username}</p>
+          <p className='dark:text-neutral-700 text-neutral-700 text-lg sm:text-xl md:text-2xl'>See your account info</p>
         </div>
       </div>
+      <div className='h-px md:hidden dark:bg-neutral-900 bg-neutral-200'/>
 
-      <SavedBlogs/> {/*for later*/}
+      <SavedBlogs/>
     </div>
   )
 }
--- FRONTEND/src/components/user/SavedBlogs.jsx
+++ FRONTEND/src/components/user/SavedBlogs.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const SavedBlogs = () => {
   return (
-    <div></div>
+    <div className='**:transition duration-150'>
+      <div className='space-y-2'>
+        <p className='dark:text-white text-xl sm:text-2xl md:text-3xl font-bold'>Saved Blogs</p>
+        <p className='dark:text-neutral-700 text-neutral-700 text-base sm:text-xl'>Below is a list of blogs you have saved</p>
+      </div>
+    </div>
   )
 }
 
