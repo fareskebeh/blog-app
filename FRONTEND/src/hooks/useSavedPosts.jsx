f277128e4e779268549d317db83cd051c3402ce6
fareskebeh <fares.kebbeh@gmail.com>
2025-11-02 18:08:58 +0300

    Implemented a custom hook that retrieves a lightweight copy of saved posts ids to compare

---
+++ FRONTEND/src/hooks/useSavedPosts.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import axiosInit from "../services/axios-init";
+
+export const useSavedPosts = () => {
+  const token = localStorage.getItem("token");
+  const [posts,setPosts] = useState(null)
+
+  const getSavedIds = (args)=> {
+
+    if(args) {
+        const cleaned= args.map((arg)=> {
+            return arg.id
+        })
+        return cleaned
+    }
+    else {
+        return
+    }
+  }
+
+  useEffect(() => {
+    const saved = localStorage.getItem("saved");
+
+    if (!saved) {
+      axiosInit.get("saved", {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+            })
+            .then((res) => {
+                const ids = getSavedIds(res.data.data)
+                localStorage.setItem("saved", JSON.stringify(ids))
+                setPosts(res.data.data)
+            })
+            .catch((err) => {
+                console.error(err)
+            });
+        }
+    }, []);
+
+  return posts ? posts : [];
+};
