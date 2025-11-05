b17c3c1b008114226c12e999fc444ee4e4d77d9c
fareskebeh <fares.kebbeh@gmail.com>
2025-11-05 22:21:14 +0300

    Full page loader

---
+++ FRONTEND/src/fallback/FullLoader.jsx
@@ -0,0 +1,9 @@
+const FullLoader= () => {
+  return (
+    <div className='flex items-center justify-center fixed z-999 inset-0 bg-white dark:bg-black'>
+      <div className='loader-2 w-10'></div>
+    </div>
+  )
+}
+
+export default FullLoader
