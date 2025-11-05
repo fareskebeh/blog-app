eba02ed976e94d2e1f4107bca75565867dc71754
fareskebeh <fares.kebbeh@gmail.com>
2025-11-05 23:10:39 +0300

    Added an avatar + style tweaks

--- FRONTEND/src/components/Comment.jsx
+++ FRONTEND/src/components/Comment.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 
-const Comment = ({ author, content, date_created }) => {
+const Comment = ({ by, avatar, content, date_created }) => {
   return (
-    <div className={`p-4 flex flex-col gap-4`}>
-      <div className={`
-        flex gap-2`}>
-        <p className={`dark:text-white font-bold 
-            `}>{author}</p>
-        <p className="text-neutral-600 dark:text-neutral-500">&#8226;</p>
-        <p className="text-neutral-600 dark:text-neutral-500">{date_created}</p>
+    <div className="flex gap-2 py-4 items-center">
+      <img className="w-12 h-12 sm:w-14 sm:w-14 rounded-full border shadow-sm dark:border-neutral-700" src={import.meta.env.VITE_API_BASE+avatar}/> 
+      <div className={`p-2 flex flex-col gap-2`}>
+        <div className={`
+          flex gap-2`}>
+          <p className={`dark:text-white font-bold 
+              `}>@{by}</p>
+          <p className="text-neutral-600 dark:text-neutral-500">&#8226;</p>
+          <p className="text-neutral-600 dark:text-neutral-500">{date_created}</p>
+        </div>
+        <p className="text-md dark:text-neutral-400">{content}</p>
       </div>
-      <p className="text-md dark:text-neutral-400">{content}</p>
     </div>
   );
 };
