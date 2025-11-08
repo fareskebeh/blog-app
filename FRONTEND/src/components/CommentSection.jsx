0c2150ed12be95e6b36b4d710872d68c83c892fe
fareskebeh <fares.kebbeh@gmail.com>
2025-11-08 19:35:04 +0300

    Redesigned the comment section + loading state

--- FRONTEND/src/components/CommentSection.jsx
+++ FRONTEND/src/components/CommentSection.jsx
@@ -16,7 +16,17 @@ const CommentSection = ({ comments = [], id }) => {
   });
 
   const postComment = () => {
+    setResponse({
+      shown: true,
+      message: "Please Wait...",
+      status: "loading"
+    })
     if (currCom.content.trim() === "") {
+      setResponse({
+        shown: false,
+        message: "",
+        status: "error"
+      })
       return;
     } else {
       axiosInit
@@ -47,39 +57,37 @@ const CommentSection = ({ comments = [], id }) => {
 
   return (
     <>
-      <div
-        className={`flex **:transition duration-150 gap-2 items-center p-2 rounded-2xl transition pointer-events-none text-white `}
-      >
+      <div className="flex flex-col md:flex-row md:*:flex-1 md:gap-8">
+
+        <div className="overflow-y-hidden md:bottom-2">
         
-      </div>
-      <div>
-        
-        <p className="text-3xl font-bold dark:text-white">Comments ({comments.length})</p>
-        <div className="relative">
-          <div className={`h-30 my-4 **:transition duration-150`}>
-            <textarea
-              rows="1"
-              className={`h-full pb-14 resize-none dark:bg-neutral-900 w-full bg-neutral-200 rounded-3xl p-4 dark:text-white placeholder-neutral-500 dark:placeholder-neutral-600 outline-none`}
-              type="text"
-              placeholder="Add a comment.."
-              onChange={(e) =>
-                setCurrCom({ ...currCom, content: e.target.value })
-              }
-            />
-            <div className="absolute rounded-bl-3xl rounded-br-3xl dark:bg-neutral-900 bg-neutral-200 bottom-0 left-0 right-0 p-2 flex justify-between items-center">
-              <p className="ml-4 dark:text-neutral-700 text-neutral-500">{currCom.content.length}/500</p>
-              
-              <button
-                onClick={() => postComment()}
-                className="transition cursor-pointer hover:opacity-90 active:opacity-80 duration-150 p-2 dark:bg-white text-white dark:text-black bg-black rounded-full"
-                >
-                <FaRegPaperPlane size={20}/>
-              </button>
+          <p className="text-3xl font-bold dark:text-white">Comments ({comments.length})</p>
+          <div className="relative z-200">
+            <div className={`h-30 my-4 **:transition duration-150`}>
+              <textarea
+                rows="1"
+                className={`h-full pb-14 resize-none dark:bg-neutral-900 w-full bg-neutral-200 rounded-3xl p-4 dark:text-white placeholder-neutral-500 dark:placeholder-neutral-600 outline-none`}
+                type="text"
+                placeholder="Add a comment.."
+                onChange={(e) =>
+                  setCurrCom({ ...currCom, content: e.target.value })
+                }
+              />
+              <div className="absolute rounded-bl-3xl rounded-br-3xl dark:bg-neutral-900 bg-neutral-200 bottom-0 left-0 right-0 p-2 flex justify-between items-center">
+                <p className="ml-4 dark:text-neutral-700 text-neutral-500">{currCom.content.length}/500</p>
+                
+                <button
+                  onClick={() => postComment()}
+                  className={`transition ${!currCom.content.trim() || response.status==="loading" ? "dark:opacity-60 opacity-20 cursor-not-allowed" : "hover:opacity-90 active:opacity-80 cursor-pointer"} duration-150 p-2 dark:bg-white text-white dark:text-black bg-black rounded-full`}
+                  >
+                  <FaRegPaperPlane size={20}/>
+                </button>
+              </div>
             </div>
           </div>
         </div>
 
-        <div>
+        <div className="md:h-50 overflow-y-auto border dark:border-neutral-800 border-neutral-300 shadow-md bg-neutral-200 dark:bg-neutral-900  px-4 rounded-xl">
           {comments.length > 0 ? (
             comments.map((comment, index) => (
               <div key={comment.id}>
@@ -92,7 +100,7 @@ const CommentSection = ({ comments = [], id }) => {
                 />
                 {index !== comments.length - 1 && (
                   <hr
-                    className={`dark:border-neutral-900 border-neutral-200`}
+                    className={`dark:border-neutral-800 transition duration-150 border-neutral-300`}
                   />
                 )}
               </div>
@@ -115,6 +123,8 @@ const CommentSection = ({ comments = [], id }) => {
             </div>
           )}
         </div>
+
+
         {
           response.shown && <Message response={response} setResponse={setResponse}/>
         }
