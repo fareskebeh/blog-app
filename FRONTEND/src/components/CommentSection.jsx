4b64656c29d3e2266e1b7e3a81d1402b51fc2178
fareskebeh <fares.kebbeh@gmail.com>
2025-11-08 20:13:25 +0300

    better textarea height + comment textarea restyling

--- FRONTEND/src/components/CommentSection.jsx
+++ FRONTEND/src/components/CommentSection.jsx
@@ -21,7 +21,7 @@ const CommentSection = ({ comments = [], id }) => {
       message: "Please Wait...",
       status: "loading"
     })
-    if (currCom.content.trim() === "") {
+    if (currCom.content.trim() === "" || currCom.content.trim().length > 500) {
       setResponse({
         shown: false,
         message: "",
@@ -57,37 +57,35 @@ const CommentSection = ({ comments = [], id }) => {
 
   return (
     <>
-      <div className="flex flex-col md:flex-row md:*:flex-1 md:gap-8">
+      <div className="flex flex-col gap-4 md:h-80 md:flex-row md:*:flex-1 md:gap-8">
 
-        <div className="overflow-y-hidden md:bottom-2">
+        <div className="overflow-y-hidden flex flex-col gap-4">
         
-          <p className="text-3xl font-bold dark:text-white">Comments ({comments.length})</p>
-          <div className="relative z-200">
-            <div className={`h-30 my-4 **:transition duration-150`}>
-              <textarea
-                rows="1"
-                className={`h-full pb-14 resize-none dark:bg-neutral-900 w-full bg-neutral-200 rounded-3xl p-4 dark:text-white placeholder-neutral-500 dark:placeholder-neutral-600 outline-none`}
-                type="text"
-                placeholder="Add a comment.."
-                onChange={(e) =>
-                  setCurrCom({ ...currCom, content: e.target.value })
-                }
-              />
-              <div className="absolute rounded-bl-3xl rounded-br-3xl dark:bg-neutral-900 bg-neutral-200 bottom-0 left-0 right-0 p-2 flex justify-between items-center">
-                <p className="ml-4 dark:text-neutral-700 text-neutral-500">{currCom.content.length}/500</p>
-                
-                <button
-                  onClick={() => postComment()}
-                  className={`transition ${!currCom.content.trim() || response.status==="loading" ? "dark:opacity-60 opacity-20 cursor-not-allowed" : "hover:opacity-90 active:opacity-80 cursor-pointer"} duration-150 p-2 dark:bg-white text-white dark:text-black bg-black rounded-full`}
-                  >
-                  <FaRegPaperPlane size={20}/>
-                </button>
-              </div>
+          <p className="text-3xl transition duration-150 font-bold dark:text-white">Comments ({comments.length})</p>
+          
+          <div className={`flex flex-1 flex-col`}>
+            <textarea
+              className={`pb-14 flex-1 md:h-full resize-none dark:bg-neutral-900 w-full bg-neutral-200 rounded-tr-3xl rounded-tl-3xl p-4 dark:text-white placeholder-neutral-500 dark:placeholder-neutral-600 outline-none transition duration-150`}
+              type="text"
+              placeholder="Add a comment.."
+              onChange={(e) =>
+                setCurrCom({ ...currCom, content: e.target.value })
+              }
+            />
+            <div className="transition duration-150 rounded-bl-3xl rounded-br-3xl dark:bg-neutral-900 bg-neutral-200 bottom-0 left-0 right-0 p-2 flex justify-between items-center">
+              <p className="ml-4 dark:text-neutral-700 text-neutral-500">{currCom.content.length}/500</p>
+              
+              <button
+                onClick={() => postComment()}
+                className={`transition ${!currCom?.content.trim() || response.status==="loading" || currCom?.content.trim().length > 500 ? "dark:opacity-60 opacity-20 cursor-not-allowed" : "hover:opacity-90 active:opacity-80 cursor-pointer"} duration-150 p-2 dark:bg-white text-white dark:text-black bg-black rounded-full`}
+                >
+                <FaRegPaperPlane size={20}/>
+              </button>
             </div>
           </div>
         </div>
 
-        <div className="md:h-50 overflow-y-auto border dark:border-neutral-800 border-neutral-300 shadow-md bg-neutral-200 dark:bg-neutral-900  px-4 rounded-xl">
+        <div className="h-full overflow-y-auto md:border dark:border-neutral-800 border-neutral-300 md:shadow-md md:bg-neutral-200 md:dark:bg-neutral-900  px-4 rounded-xl">
           {comments.length > 0 ? (
             comments.map((comment, index) => (
               <div key={comment.id}>
@@ -100,7 +98,7 @@ const CommentSection = ({ comments = [], id }) => {
                 />
                 {index !== comments.length - 1 && (
                   <hr
-                    className={`dark:border-neutral-800 transition duration-150 border-neutral-300`}
+                    className={`md:dark:border-neutral-800 dark:border-neutral-900 transition duration-150 md:border-neutral-300 border-neutral-200`}
                   />
                 )}
               </div>
