60e74379331b08e536139697329349e08abce6e9
fareskebeh <fares.kebbeh@gmail.com>
2025-12-17 14:54:04 +0300

    Error states for home + img

---
+++ FRONTEND/src/fallback/error/NotFound.jsx
@@ -0,0 +1,23 @@
+import {TbError404Off} from "react-icons/tb"
+
+const NotFound = () => {
+  return (
+    <div className="relative **:transition duration-150 py-4">
+        <div className="flex flex-col gap-2">
+        {
+            Array.from({length: 2} ,(_,i)=> (
+                <div key={i} className="h-25 w-full dark:bg-neutral-900 bg-neutral-200 rounded-2xl"></div>
+            ))
+        }
+        </div>
+        <div className="flex items-center justify-center absolute inset-0 bg-linear-to-t dark:from-neutral-950 dark:via-neutral-950/60 from-white rounded-4xl">
+            <div className="w-full gap-4 h-40 dark:text-neutral-500 text-neutral-500 **:transition duration-150 rounded-3xl flex flex-col items-center justify-center">
+                <TbError404Off color="#ff4040" size={48}/>
+                <p>Could not find post, Try again with different keywords</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default NotFound
---
+++ FRONTEND/src/fallback/error/OtherBlogsError.jsx
@@ -0,0 +1,24 @@
+import { MdErrorOutline } from "react-icons/md"
+
+const OtherBlogsError = () => {
+  return (
+    <div className="relative **:transition duration-150 py-4">
+        <div className="flex flex-col gap-2">
+        {
+            Array.from({length: 2} ,(_,i)=> (
+                <div key={i} className="h-25 w-full dark:bg-neutral-900 bg-neutral-200 rounded-2xl"></div>
+            ))
+        }
+        </div>
+        <div className="flex items-center justify-center absolute inset-0 bg-linear-to-t dark:from-neutral-950 dark:via-neutral-950/60 from-white rounded-4xl">
+            <div className="w-full gap-4 h-40 dark:text-neutral-500 text-neutral-500 **:transition duration-150 rounded-3xl flex flex-col items-center justify-center">
+                <MdErrorOutline color="#ff4040" size={36}/>
+                <p>Could not load posts, Try again</p>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default OtherBlogsError
