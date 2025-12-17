2fcd296132d4b41962764f96d47ceb1eb93ed19d
fareskebeh <fares.kebbeh@gmail.com>
2025-12-17 17:35:19 +0300

    Click outside handler

--- FRONTEND/src/reusables/Dropdown.jsx
+++ FRONTEND/src/reusables/Dropdown.jsx
@@ -1,17 +1,31 @@
 import { HiChevronDown } from 'react-icons/hi'
 import { AnimatePresence, motion as Motion } from 'framer-motion'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 const Dropdown = ({data, defaultVal}) => {
+
     const[open,setOpen] = useState(false)
     const[newVal,setNewVal]=useState(defaultVal)
-  return (
-    <div onClick={()=>setOpen(!open)} className='**:transition duration-150 dark:bg-neutral-900 bg-neutral-100 cursor-pointer flex w-30 justify-between items-center p-2 rounded-xl text-base relative'>
+    const dropRef = useRef(null)
+    useEffect(()=> {
+        const clickOutside = (e)=> {
+                if(dropRef.current && !dropRef.current.contains(e.target)) {
+                    setOpen(false)
+            }
+        }
+
+        document.addEventListener('click', clickOutside)
+
+        return ()=> removeEventListener('click', clickOutside)
+    },[])
+  
+    return (
+    <div ref={dropRef} onClick={()=>setOpen(!open)} className='**:transition duration-150 dark:bg-neutral-900 bg-neutral-100 cursor-pointer flex w-30 justify-between items-center p-2 rounded-xl text-base relative'>
         <p className='select-none'>{newVal}</p>
         <HiChevronDown className={`${open ? "transform:rotate-180" : ""}`}/>
         <AnimatePresence>
         { open &&
-        <Motion.div onClick={(e)=> e.stopPropagation()} initial={{opacity:0, y:-10}} animate={{opacity:1, y:0}} exit={{opacity:0, y:-10}} transition={{duration:0.1}} className='absolute p-2 shadow-md bg-neutral-100 rounded-xl top-[110%] inset-x-0 flex flex-col gap-2 dark:bg-neutral-900 max-h-60 overflow-y-auto'>
+        <Motion.div onClick={(e)=> e.stopPropagation()} initial={{opacity:0, y:-10}} animate={{opacity:1, y:0}} exit={{opacity:0, y:-10}} transition={{duration:0.1}} className='absolute z-999 p-2 shadow-lg bg-neutral-100 rounded-xl top-[110%] inset-x-0 flex flex-col gap-2 dark:bg-neutral-900 max-h-60 overflow-y-auto'>
             {
                 data.map((d,index)=> (
                     <p onClick={()=> {setNewVal(d); setOpen(false)}} className={`p-2 select-none rounded-lg ${newVal===d ? "bg-black text-white dark:text-black dark:bg-white" : "hover:bg-neutral-200 dark:hover:bg-neutral-800"}`} key={index}>{d}</p>
